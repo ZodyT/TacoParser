8bddbfec51f827e02cf0577bfd19b1a1b58d1d66
ZodyT <Zoraida1988@yahoo.com>
2020-12-01 19:16:00 -0600

    Variables

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -17,17 +17,37 @@
             // If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log that and return null
+                logger.LogWarning("Less than 3");// Log that and return null
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
             // grab the latitude from your array at index 0
+            double TBlat = 0;
+
+            if (double.TryParse(cells[0], out TBlat) == false)
+            {
+                logger.LogError("Can't parse latitude as double");
+
+            }
+
             // grab the longitude from your array at index 1
+            double TBlong = 0;
+
+            if (double.TryParse(cells[1], out TBlong) == false)
+            {
+                logger.LogError("Can't parse longitude as double");
+
+            }
+
             // grab the name from your array at index 2
+            var name = cells[2];
 
             // Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
+            TBlat = Convert.Todouble();
+
+
 
             // You'll need to create a TacoBell class
             // that conforms to ITrackable
